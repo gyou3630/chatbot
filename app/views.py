20767a722e86480588d8c52817ba6edc43c28636
JIYOU <gu@gimjiyuui-MacBook-Pro.local>
2018-09-16 18:00:51 +0900

    Type buttons to text

--- app/views.py
+++ app/views.py
@@ -6,7 +6,6 @@ from django.http import JsonResponse
 def keyboard(request):
     return JsonResponse(
         {
-            'type': 'buttons',
-            'buttons': ['학식', '내일의 학식', '도서관']
+            'type': 'text'
         }
     )
